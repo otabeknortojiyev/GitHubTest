d77974d202d8a9852a50596a72250ba6e0a9f5ec
Отабек Нортожиев <otashmotash@gmail.com>
2024-11-06 00:04:12 +0500

    First commit in second branch

--- app/src/main/java/uz/yayra/otabek/githubtest/MainActivity.kt
+++ app/src/main/java/uz/yayra/otabek/githubtest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
 import uz.yayra.otabek.githubtest.ui.theme.GitHubTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GitHubTestTheme {
-
+                Text("2")
             }
         }
     }
